2c0cf8faa22a34831e9972eb1f24be3bbc344f3d
Sergey Palcheh <minergenon@altlinux.org>
2024-10-17 14:53:03 +0600

    Added data.py

---
+++ data.py
@@ -0,0 +1,7 @@
+BASE_URL = "https://stellarburgers.nomoreparties.site/api"
+
+REGISTER = f"{BASE_URL}/auth/register"
+LOGIN = f"{BASE_URL}/auth/login"
+USER = f"{BASE_URL}/auth/user"
+ORDERS = f"{BASE_URL}/orders"
+INGREDIENTS = f"{BASE_URL}/ingredients"
