0d91eb78c808bdb7b0a43196950db0a8dc03a197
Sergey Palcheh <minergenon@altlinux.org>
2024-10-17 15:18:53 +0600

    Added test_user_login

---
+++ tests/test_user_login.py
@@ -0,0 +1,37 @@
+import allure
+import pytest
+from api_methods import ApiMethods
+from expected_responses import *
+from helpers import ResponseChecker
+
+
+@allure.feature("Авторизация пользователя")
+class TestUserLogin:
+
+    @allure.title("Успешная авторизация существующего пользователя")
+    @pytest.mark.positive
+    def test_login_existing_user(self, create_user):
+        email, password, _ = create_user
+        response = ApiMethods.login_user(email, password)
+        assert ResponseChecker.check_status_code(response, SUCCESS_CODE) and \
+               ResponseChecker.check_field_exists(response, 'accessToken'), \
+            f"Не удалось авторизоваться. Код ответа: {response.status_code}, тело ответа: {response.json()}"
+
+    @allure.title("Попытка авторизации с неверными данными")
+    @pytest.mark.negative
+    @pytest.mark.parametrize("email,password", [
+        ("wrong@email.com", "correctpassword"),
+        ("correct@email.com", "wrongpassword"),
+    ])
+    def test_login_with_invalid_credentials(self, create_user, email, password):
+        correct_email, correct_password, _ = create_user
+        if email == "correct@email.com":
+            email = correct_email
+        if password == "correctpassword":
+            password = correct_password
+
+        response = ApiMethods.login_user(email, password)
+        assert ResponseChecker.check_status_code(response, UNAUTHORIZED_CODE) and \
+               ResponseChecker.check_response_field(response, 'message', LOGIN_FAILED['message']), \
+            (f"Неожиданный ответ при попытке авторизации с неверными данными. "
+             f"Код ответа: {response.status_code}, тело ответа: {response.json()}")
